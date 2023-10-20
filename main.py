8405e521f5786e22b717c24ad2f98e1f704296ab
rohanpatil018 <148521216+rohanpatil018@users.noreply.github.com>
2023-10-20 10:55:54 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+keys=input("enter keys seperated by space: ").split()
+num_lists=int(input("enter the number of values lists: "))
+result_dict={}
+for i in range(num_lists):
+    values=input(f"enter values sepearted by space for {keys[1]}: ").split()
+    result_dict[keys[i]]=values
+print("/ncreated dictionary:")
+print(result_dict)
